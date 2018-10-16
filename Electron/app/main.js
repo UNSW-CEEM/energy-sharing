7b8189b26f8eec43f5de82ce7313bc232f0b1f45
David Martin <davidmartindevelopment@gmail.com>
2018-10-16 12:01:59 +1100

    Added a default vue application. May change structure more yet.

--- Electron/app/main.js
+++ Electron/app/main.js
@@ -1,55 +1,30 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 
-  // Keep a global reference of the window object, if you don't, the window will
-  // be closed automatically when the JavaScript object is garbage collected.
-  let win
+let win = null;
 
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
+app.on('ready', function () {
 
-    // and load the index.html of the app.
-    win.loadFile('index.html')
+  // Initialize the window to our specified dimensions
+  win = new BrowserWindow({width: 1000, height: 600});
 
-    // Open the DevTools.
-    // win.webContents.openDevTools()
+  // Specify entry point to default entry point of vue.js
+  win.loadURL('http://localhost:8080');
 
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      win = null
-    })
+  // Remove window once app is closed
+  win.on('closed', function () {
+  win = null;
+  });
 
-    var pyshell = require('python-shell');
-    pyshell.run('../../Flask/engine.py', function(err, results) {
-        if (err) console.log(err);
-    });
-
+});
+//create the application window if the window variable is null
+app.on('activate', () => {
+  if (win === null) {
+  createWindow()
   }
-
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow)
-
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
-
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow()
-    }
-  })
-
-  // In this file you can include the rest of your app's specific main process
-  // code. You can also put them in separate files and require them here.
+})
+//quit the app once closed
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+  app.quit();
+  }
+npm });
