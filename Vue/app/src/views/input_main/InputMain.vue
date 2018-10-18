e93043542a484da853294989a510c72a02b9541d
David Martin <davidmartindevelopment@gmail.com>
2018-10-18 13:13:11 +1100

    Moving some files around. Setting up basic routes.

---
+++ Vue/app/src/views/input_main/InputMain.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="inputmain">
+        <p>Hello There</p>
+        <li><router-link to="/model">Model</router-link></li>
+        <li><router-link to="/data">Data</router-link></li>
+        <router-view></router-view>
+    </div>
+</template>
---
+++ Vue/app/src/views/input_main/input_pages/Data.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>This View is named {{ view_name }}</h1>
+</template>
+
+<script>
+    export default {
+        name: "Data",
+        data () {
+            return {
+                view_name: this.$options.name,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
