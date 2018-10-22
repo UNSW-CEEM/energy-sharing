aeeed0784cac2c3dede1c287411ec00e9a8ed538
David Martin <davidmartindevelopment@gmail.com>
2018-10-22 14:21:49 +1100

    Adding getters

--- Vue/app/src/pages/inputs/Model.vue
+++ Vue/app/src/pages/inputs/Model.vue
@@ -1,5 +1,19 @@
 <template>
-    <h1>This View is named {{ view_name }}</h1>
+    <div>
+        <h1>This View is named {{ view_name }}</h1>
+        <ul>
+            <li
+                v-for="ex in examples"
+                :key="ex.id">
+                <span class="name">{{ ex.name }}</span>
+            </li>
+            <li
+                v-for="ex in examplesGetter"
+                :key="ex.id">
+                <span class="name">{{ ex.name }}</span>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -8,6 +22,14 @@
         data () {
             return {
                 view_name: this.$options.name,
+            }
+        },
+        computed: {
+            examples() {
+                return this.$store.state.examples;
+            },
+            examplesGetter() {
+                return this.$store.getters.examplesGetter;
             }
         }
     }
--- Vue/app/src/store/store.js
+++ Vue/app/src/store/store.js
@@ -6,22 +6,21 @@ Vue.use(Vuex)
 export const store = {
     state: {
         examples: [
-            {
-                id: 0,
-                name: "Example 1"
-            },
-            {
-                id: 1,
-                name: "Example 2"
-            },
-            {
-                id: 3,
-                name: "Example 3"
-            },
-            {
-                id: 4,
-                name: "Example 4"
-            }
+            { id: 0, name: "Example 1" },
+            { id: 1, name: "Example 2" },
+            { id: 3, name: "Example 3" },
+            { id: 4, name: "Example 4" }
         ]
+    },
+
+    getters: {
+        examplesGetter: (state) => {
+            return state.examples.map(example => {
+                return {
+                    id: example.id,
+                    name: example.name + 'but changed by getter'
+                }
+            });
+        }
     }
 }
