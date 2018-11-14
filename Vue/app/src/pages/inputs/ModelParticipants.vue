39629b86d748ed45380e542809e497eb79f78d14
Luke Marshall <lukasmarshall@gmail.com>
2018-11-14 15:56:18 +1100

    simple simple table

--- Vue/app/src/pages/inputs/ModelParticipants.vue
+++ Vue/app/src/pages/inputs/ModelParticipants.vue
@@ -2,7 +2,21 @@
 <template>
     <div>
         <h1>{{ view_name }}</h1>
+         <table style="width:100%">
+            <tr>
+                <th>Name</th>
+                <th>kWh</th>
+                
+            </tr>
 
+            <tr v-for="row in rows">
+                <td>{{row.name}}</td>
+                <input v-model="row.kWh">
+            </tr>
+
+        </table> 
+        
+        <button v-on:click="add_row()">ADD</button>
     </div>
 </template>
 
@@ -12,9 +26,21 @@
         name: "Participants",
         components: {
         },
+        methods:{
+            add_row(){
+                this.rows.push({
+                    kWh:2,
+                    name:"Luke"
+                })
+            }
+        },
         data () {
             return {
                 view_name: this.$options.name,
+                rows:[{
+                    kWh:1,
+                    name:'Luke'
+                }],
             }
         }
     }
