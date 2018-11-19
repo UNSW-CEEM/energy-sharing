a75d92d8cb90905b366b88a23d521875f569bca7
David Martin <davidmartindevelopment@gmail.com>
2018-11-19 11:31:47 +1100

    Updating Solar inputs

--- Vue/app/src/pages/inputs/ModelSolar.vue
+++ Vue/app/src/pages/inputs/ModelSolar.vue
@@ -1,42 +1,79 @@
 <template>
-    <div>
+     <div>
         <h1>{{ view_name }}</h1>
-        <h4>Below Are Inputs For Store</h4>
-        <form>
-            <p>Sharing Algorithm:</p>
-            <select v-model="sharing_algorithm">
-                <option value="" disabled selected hidden>Select Your Sharing Algorithm</option>
-                <option value="Algorithm 0">Algorithm 0</option>
-                <option value="Algorithm 1">Algorithm 1</option>
-                <option value="Algorithm 2">Algorithm 2</option>
-                <option value="Algorithm 3">Algorithm 3</option>
-            </select>
-            <br>
-            <p>Scaling Factor:</p>
-            <input placeholder="Enter Your Scaling Factor">
-            <br>
-            <p>Select Your Data Source</p>
-            <select v-model="data_source">
-                <option value="" disabled selected hidden>Select Your Data Source</option>
-                <option
-                    v-for="source in input_data_source"
-                    :key="source.id"
-                    :value="source.name">
-                    {{ source.name }}
-                </option>
-            </select>
-        </form>
+        <span
+            v-for="input in input_data"
+            :key="input.id">{{ input.display_text }}
+
+            <SimpleNumberInput
+                v-if="input.tag==='my_number'"
+                v-model="input.value"
+                :my_placeholder="input.placeholder"/>
+
+            <SimpleDropdown v-else-if="input.tag==='my_dropdown'"
+                v-model="input.value"
+                :my_options="my_options[input.dropdown_key]"
+                :my_placeholder="input.placeholder"/>
+        <br>
+        </span>
     </div>
 </template>
 
 <script>
+    import SimpleNumberInput from '@/components/SimpleNumberInput.vue';
+    import SimpleDropdown from '@/components/SimpleDropdown.vue';
 
     export default {
         name: "CentralSolar",
 
+        components: {
+            SimpleDropdown,
+            SimpleNumberInput,
+        },
+
         data () {
             return {
                 view_name: this.$options.name,
+
+                input_data: [
+                    {
+                        id: 0,
+                        name: "data_source",
+                        display_text: "Data Source  ",
+                        value: "",
+                        dropdown_key: "data_source",
+                        placeholder: "Select One",
+                        tag:"my_dropdown",
+                    },
+                    {
+                        id: 1,
+                        name: "scaling_factor",
+                        display_text: "Scaling Factor ",
+                        value: "",
+                        placeholder: "Decimal Scaling Factor",
+                        tag:"my_number"
+                    },
+                    {
+                        id: 2,
+                        name:"sharing_algorithm",
+                        display_text: "Sharing Algorithm ",
+                        value: "",
+                        dropdown_key: "sharing_algorithm",
+                        placeholder: "Select One",
+                        tag:"my_dropdown"
+                    },
+                ],
+
+                my_options: {
+                    data_source: {
+                        option_one: "Option 1",
+                        option_two: "ABC"
+                    },
+                    sharing_algorithm: {
+                        option_one: "Option 1",
+                        option_two: "ABC"
+                    },
+                }
             }
         },
         computed: {
