28f9b9a9e977b785067369c7e165b487508a9d4b
David Martin <davidmartindevelopment@gmail.com>
2018-11-19 14:15:35 +1100

    Updating participants inputs

--- Vue/app/src/pages/inputs/ModelParticipants.vue
+++ Vue/app/src/pages/inputs/ModelParticipants.vue
@@ -2,45 +2,143 @@
 <template>
     <div>
         <h1>{{ view_name }}</h1>
-         <table style="width:100%">
+         <table>
             <tr>
-                <th>Name</th>
-                <th>kWh</th>
-                
+                <th
+                    v-for="header in table_headers"
+                    :key="header.header_id"
+                    :value="header.name">{{ header.name }}</th>
             </tr>
-
-            <tr v-for="row in rows">
-                <td>{{row.name}}</td>
-                <input v-model="row.kWh">
+            <tr>
+                <td
+                    v-for="header in table_headers"
+                    :key="header.header_id">{{ header.additional_text }}</td>
             </tr>
-
-        </table> 
-        
-        <button v-on:click="add_row()">ADD</button>
+            <tr
+                v-for="row in table_rows"
+                :key="row.row_id">
+                <td v-for="input in row.row_inputs"
+                :key="input.col_id"
+                >
+                    <SimpleNumberInput
+                            v-if="input.tag==='my_number'"
+                            v-model="input.value"
+                            :my_placeholder="input.placeholder"/>
+                    <SimpleDropdown v-else-if="input.tag==='my_dropdown'"
+                                    v-model="input.value"
+                                    :my_options="my_options[input.dropdown_key]"
+                                    :my_placeholder="input.placeholder"/>
+                </td>
+            </tr>
+            <button @click="add_row()">Add Row</button>
+        </table>
     </div>
 </template>
 
 <script>
+    import SimpleNumberInput from '@/components/SimpleNumberInput.vue';
+    import SimpleDropdown from '@/components/SimpleDropdown.vue';
 
     export default {
         name: "Participants",
         components: {
+            SimpleNumberInput,
+            SimpleDropdown
         },
-        methods:{
-            add_row(){
-                this.rows.push({
-                    kWh:2,
-                    name:"Luke"
-                })
-            }
-        },
+
         data () {
             return {
                 view_name: this.$options.name,
-                rows:[{
-                    kWh:1,
-                    name:'Luke'
-                }],
+                table_headers: [
+                    {id: 0, name: "Participant ID", additional_text:"ID"},
+                    {id: 1, name: "Participant Type", additional_text:"Type"},
+                    {id: 2, name: "Tariff Type", additional_text:"Select One"},
+                    {id: 3, name: "Load Data", additional_text:"Select One"},
+                    {id: 4, name: "Solar Data", additional_text:"Select One"},
+                    {id: 5, name: "Solar Scaling", additional_text:"Input Number"},
+                    {id: 6, name: "Battery", additional_text:"Select One"},
+                ],
+
+                table_rows: [],
+
+                my_options: {
+                    example: {
+                        option_one: "Option One",
+                        option_two: "Option Two",
+                    },
+                    who_pays: {
+                        option_one: "Option 1",
+                        option_two: "Option 2",
+                    }
+                },
+            }
+        },
+
+        created() {
+            this.add_row()
+        },
+
+        methods: {
+            add_row() {
+                let array_length = this.table_rows.length;
+                let new_row = {
+                    row_id: array_length,
+                    row_inputs: [
+                        {
+                            id: 0,
+                            name: "participant_id",
+                            tag: "my_number",
+                            value:"",
+                            placeholder:"ID",
+                        },
+                        {
+                            id: 1,
+                            name: "participant_type",
+                            tag: "my_number",
+                            value:"",
+                            placeholder:"Type",
+                        },
+                        {
+                            id: 2,
+                            name: "tariff_type",
+                            tag: "my_dropdown",
+                            value:"",
+                            dropdown_key:"tariff_options",
+                            placeholder:"Select One",
+                        },
+                        {
+                            id: 3,
+                            name: "load_data",
+                            tag: "my_number",
+                            value:"",
+                            placeholder:"Select One",
+                        },
+                        {
+                            id: 4,
+                            name: "solar_data",
+                            tag: "my_number",
+                            value:"",
+                            placeholder:"Select One",
+                        },
+                        {
+                            id: 5,
+                            name: "solar_scaling",
+                            tag: "my_number",
+                            value:"",
+                            placeholder:"Input Number",
+                        },
+                        {
+                            id: 6,
+                            name: "battery_type",
+                            tag: "my_dropdown",
+                            value:"",
+                            dropdown_key:"battery_options",
+                            placeholder:"Select Battery",
+                        },
+                    ]
+                };
+
+                this.table_rows.push(new_row);
             }
         }
     }
