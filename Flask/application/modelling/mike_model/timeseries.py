b21f3e23fbf9b9332c59b5a9f7fcb62d310aff4b
Luke Marshall <lukasmarshall@gmail.com>
2019-03-08 21:13:58 +0900

    Made date_times a private variable

--- Flask/application/modelling/mike_model/timeseries.py
+++ Flask/application/modelling/mike_model/timeseries.py
@@ -9,20 +9,20 @@ class Timeseries:
                  dst_lookup,
                  dst_region
                  ):
-        self.date_times = load.index
+        self._date_times = load.index
         
         self.interval = \
             pd.to_timedelta(
                 pd.tseries.frequencies.to_offset(
-                    pd.infer_freq(self.date_times)
+                    pd.infer_freq(self._date_times)
                 )).total_seconds()
-        self.num_days = int(len(self.date_times) * self.interval / (24 * 60 * 60))
+        self.num_days = int(len(self._date_times) * self.interval / (24 * 60 * 60))
         # Set up weekdays and weekends
         self._days = {
-            'day': self.date_times[self.date_times.weekday.isin([0, 1, 2, 3, 4])],
-            'end': self.date_times[self.date_times.weekday.isin([5, 6])],
-            'both': self.date_times}
-        self.step_ts = pd.Series(self.date_times)
+            'day': self._date_times[self._date_times.weekday.isin([0, 1, 2, 3, 4])],
+            'end': self._date_times[self._date_times.weekday.isin([5, 6])],
+            'both': self._date_times}
+        self.step_ts = pd.Series(self._date_times)
 
         # Set up summer and winter periods for daylight savings:
         # NB This is negative because it is applied to tariff period start and end times,
@@ -30,17 +30,17 @@ class Timeseries:
         # https://www.xkcd.com/1883/
         self.dst_reverse_shift = pd.DateOffset(hours=-1)
         self.seasonal_time = {
-            'winter': self.date_times[0:0],
-            'summer': self.date_times[0:0]
+            'winter': self._date_times[0:0],
+            'summer': self._date_times[0:0]
         }
 
         start_label = dst_region + '_start'
         end_label = dst_region + '_end'
 
-        for year in self.date_times.year.drop_duplicates().tolist():
+        for year in self._date_times.year.drop_duplicates().tolist():
             dst_start = pd.Timestamp(dst_lookup.loc[year, start_label])
             dst_end = pd.Timestamp(dst_lookup.loc[year, end_label])
-            tsy = self.date_times[self.date_times.year == year]
+            tsy = self._date_times[self._date_times.year == year]
             if dst_start < dst_end:
                 self.seasonal_time['winter'] = \
                     self.seasonal_time['winter'].join(tsy[(tsy >= pd.Timestamp('1/01/' + str(year) + ' 00:00:00'))
@@ -70,10 +70,10 @@ class Timeseries:
         return steps_so_far_today
     
     def get_num_steps(self):
-        return len(self.date_times)
+        return len(self._date_times)
     
     def get_date_times(self):
-        return [dt for dt in self.date_times]
+        return [dt for dt in self._date_times]
     
     def get_seasonal_times(self, season, weekday='both'):
         """
