1ce4993e54d16eb30faba5fdd574a7930aa2f255
Luke Marshall <lukasmarshall@gmail.com>
2019-03-13 11:34:15 +0900

    More spec file changes

--- Flask/package.sh
+++ Flask/package.sh
@@ -1,7 +1,1 @@
-# Best to do packaging from system python (due to current venv / pyinstaller bug). So lets do that. 
-# Generate a requirements.txt from the pipfile.lock
-pipenv lock -r > requirements.txt
-# Install the requirements in the system python
-pip3 install -r requirements.txt
-# Run pyinstaller
-pyinstaller test.py --onedir --noupx --debug
+pyinstaller run-dev-final.spec --additional-hooks-dir hooks
