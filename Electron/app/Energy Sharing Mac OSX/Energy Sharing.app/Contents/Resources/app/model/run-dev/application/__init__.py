72692b3c96e0c40183e8befcbe847ac0203511f0
Luke Marshall <lukasmarshall@gmail.com>
2019-05-06 10:49:16 +0900

    Deleted compiled versions so that file search works

--- Electron/app/Energy Sharing Mac OSX/Energy Sharing.app/Contents/Resources/app/model/run-dev/application/__init__.py
+++
@@ -1,2 +0,0 @@
-from application import api
-app = api.app
--- Electron/app/Energy Sharing Mac OSX/model/run-dev/application/modelling/ui_interfaces/central_solar.py
+++
@@ -1,7 +0,0 @@
-class CentralSolar:
-    def __init__(self, folder_routes):
-        self.luomi_dir = folder_routes.get_route("luomi_dir")
-
-        self.data_source = None
-        self.scaling_factor = None
-        self.sharing_algorithm = None
