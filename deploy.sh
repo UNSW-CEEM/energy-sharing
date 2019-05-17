6d01420a5b3f171bccbaf3acb0ee83c072939b36
Luke Marshall <lukasmarshall@gmail.com>
2019-05-17 10:50:14 +0900

    Ready for OSX Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,25 @@
-# cd Vue/app
-# echo "Installing Vue Dependencies"
-# npm install
-# echo "Building Vue app"
-# npm run build
-# cd ..
-# cd ..
-# echo "Removing Previous Builds"
-# rm -r Flask/dist
-# rm -r Flask/build
-# cd Flask
-# echo "Compiling Python Backend as standalone executable."
-# pyinstaller deploy.spec --additional-hooks-dir hooks
-# echo "Deleting previous versions of the compiled backend from the electron folder."
-# cd ..
-# rm -r Electron/app/model/*
-# echo "Copying the flask backend into the electron application folder."
-# mv Flask/dist/run-dev Electron/app/model/run-dev
-# echo "Creating OSX Application Bundle"
-# electron-packager Electron/app EnergySharing
-# echo "Copying run command into the containing folder."
-# cp ./deploy_extras/* ./EnergySharing-darwin-x64
+cd Vue/app
+echo "Installing Vue Dependencies"
+npm install
+echo "Building Vue app"
+npm run build
+cd ..
+cd ..
+echo "Removing Previous Builds"
+rm -r Flask/dist
+rm -r Flask/build
+cd Flask
+echo "Compiling Python Backend as standalone executable."
+pyinstaller deploy.spec --additional-hooks-dir hooks
+echo "Deleting previous versions of the compiled backend from the electron folder."
+cd ..
+rm -r Electron/app/model/*
+echo "Copying the flask backend into the electron application folder."
+mv Flask/dist/run-dev Electron/app/model/run-dev
+echo "Creating OSX Application Bundle"
+electron-packager Electron/app EnergySharing
+echo "Copying run command into the containing folder."
+cp ./deploy_extras/* ./EnergySharing-darwin-x64
 echo "Setting executable permissions on the run command."
 chmod +x ./EnergySharing-darwin-x64/EnergySharing.command
 echo "Moving the .app file into a subfolder"
