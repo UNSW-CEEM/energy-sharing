9ba33214823439b2b0a8ee888cf79569e111e882
luke-marshall <lukasmarshall@gmail.com>
2019-05-27 08:18:55 +0900

    Removed compiled windows version from repo

--- EnergySharing/EnergySharing.bat
+++
@@ -1,6 +0,0 @@
-echo "Ending other instances of the Backend Calculator"
-taskkill /f /im run-dev.exe
-echo "Starting Backend Calculator"
-start .\EnergySharing-win32-ia32\resources\app\model\run-dev\run-dev.exe
-echo "Starting Frontend Application"
-start .\EnergySharing-win32-ia32\EnergySharing.exe
